9aa4c4ce06d99051270aca084022373802a01198
Yevhen Ivanov <yevhen.ivanov@ab-soft.net>
2019-06-09 15:23:59 +0300

    hp scroll block

--- src/js/components/newsmakersBlock/index.js
+++ src/js/components/newsmakersBlock/index.js
@@ -35,50 +35,174 @@ class NewsmakersBlock extends React.Component {
                         </div>
                     </div>
                     <div className="scroll-newsmakers-block">
-                        <Scrollbar className="test-scroll list-news">
-                            <div className="select-news">
-                                <Link to='/'>Man UTD January Transfer life</Link>
-                                <p><img src="img/time-logo.png" alt=""/>2h<span>Rugby Union</span></p>
+                        <Scrollbar className="list-news">
+                            <div className="scroll-select-news">
+                                <div className="scroll-select-news__img">
+                                    <img src="img/sporttime.jpg" alt=""/>
+                                </div>
+                                <div className="scroll-select-news__preview">
+                                    <div className="scroll-select-news__preview__title">
+                                        <h4>Mauricio Pochettino praised Tottenham's</h4>
+                                        <p>opposed the football fan zone on the territory of the university. opposed the football fan zone on the territory of the university.</p>
+                                    </div>
+                                    <div className="scroll-select-news__preview__info">
+                                        <p><img src="img/play_icon.png" alt=""/><img src="img/time-logo.png" alt=""/>15 sec/min ago<span>Rugby Union</span></p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="select-news">
-                                <Link to='/'>Man UTD January Transfer life</Link>
-                                <p><img src="img/time-logo.png" alt=""/>2h<span>Rugby Union</span></p>
+                            <div className="scroll-select-news">
+                                <div className="scroll-select-news__img">
+                                    <img src="img/JMP_Bristol_Rugby_v_Bedford_Blues_RT0307.jpg" alt=""/>
+                                </div>
+                                <div className="scroll-select-news__preview">
+                                    <div className="scroll-select-news__preview__title">
+                                        <h4>Mauricio Pochettino praised Tottenham's</h4>
+                                        <p>opposed the football fan zone on the territory of the university. opposed the football fan zone on the territory of the university.</p>
+                                    </div>
+                                    <div className="scroll-select-news__preview__info">
+                                        <p><img src="img/play_icon.png" alt=""/><img src="img/time-logo.png" alt=""/>15 sec/min ago<span>Rugby Union</span></p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="select-news">
-                                <Link to='/'>Man UTD January Transfer life</Link>
-                                <p><img src="img/time-logo.png" alt=""/>2h<span>Rugby Union</span></p>
+                            <div className="scroll-select-news">
+                                <div className="scroll-select-news__img">
+                                    <img src="img/Minis-landscape2.jpg" alt=""/>
+                                </div>
+                                <div className="scroll-select-news__preview">
+                                    <div className="scroll-select-news__preview__title">
+                                        <h4>Mauricio Pochettino praised Tottenham's</h4>
+                                        <p>opposed the football fan zone on the territory of the university. opposed the football fan zone on the territory of the university.</p>
+                                    </div>
+                                    <div className="scroll-select-news__preview__info">
+                                        <p><img src="img/play_icon.png" alt=""/><img src="img/time-logo.png" alt=""/>15 sec/min ago<span>Rugby Union</span></p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="select-news">
-                                <Link to='/'>Man UTD January Transfer life</Link>
-                                <p><img src="img/time-logo.png" alt=""/>2h<span>Rugby Union</span></p>
+                            <div className="scroll-select-news">
+                                <div className="scroll-select-news__img">
+                                    <img src="img/Minis-landscape.png" alt=""/>
+                                </div>
+                                <div className="scroll-select-news__preview">
+                                    <div className="scroll-select-news__preview__title">
+                                        <h4>Mauricio Pochettino praised Tottenham's</h4>
+                                        <p>opposed the football fan zone on the territory of the university. opposed the football fan zone on the territory of the university.</p>
+                                    </div>
+                                    <div className="scroll-select-news__preview__info">
+                                        <p><img src="img/play_icon.png" alt=""/><img src="img/time-logo.png" alt=""/>15 sec/min ago<span>Rugby Union</span></p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="select-news">
-                                <Link to='/'>Man UTD January Transfer life</Link>
-                                <p><img src="img/time-logo.png" alt=""/>2h<span>Rugby Union</span></p>
+                            <div className="scroll-select-news">
+                                <div className="scroll-select-news__img">
+                                    <img src="img/sporttime.jpg" alt=""/>
+                                </div>
+                                <div className="scroll-select-news__preview">
+                                    <div className="scroll-select-news__preview__title">
+                                        <h4>Mauricio Pochettino praised Tottenham's</h4>
+                                        <p>opposed the football fan zone on the territory of the university. opposed the football fan zone on the territory of the university.</p>
+                                    </div>
+                                    <div className="scroll-select-news__preview__info">
+                                        <p><img src="img/play_icon.png" alt=""/><img src="img/time-logo.png" alt=""/>15 sec/min ago<span>Rugby Union</span></p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="select-news">
-                                <Link to='/'>Man UTD January Transfer life</Link>
-                                <p><img src="img/time-logo.png" alt=""/>2h<span>Rugby Union</span></p>
+                            <div className="scroll-select-news">
+                                <div className="scroll-select-news__img">
+                                    <img src="img/JMP_Bristol_Rugby_v_Bedford_Blues_RT0307.jpg" alt=""/>
+                                </div>
+                                <div className="scroll-select-news__preview">
+                                    <div className="scroll-select-news__preview__title">
+                                        <h4>Mauricio Pochettino praised Tottenham's</h4>
+                                        <p>opposed the football fan zone on the territory of the university. opposed the football fan zone on the territory of the university.</p>
+                                    </div>
+                                    <div className="scroll-select-news__preview__info">
+                                        <p><img src="img/play_icon.png" alt=""/><img src="img/time-logo.png" alt=""/>15 sec/min ago<span>Rugby Union</span></p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="select-news">
-                                <Link to='/'>Man UTD January Transfer life</Link>
-                                <p><img src="img/time-logo.png" alt=""/>2h<span>Rugby Union</span></p>
+                            <div className="scroll-select-news">
+                                <div className="scroll-select-news__img">
+                                    <img src="img/Minis-landscape2.jpg" alt=""/>
+                                </div>
+                                <div className="scroll-select-news__preview">
+                                    <div className="scroll-select-news__preview__title">
+                                        <h4>Mauricio Pochettino praised Tottenham's</h4>
+                                        <p>opposed the football fan zone on the territory of the university. opposed the football fan zone on the territory of the university.</p>
+                                    </div>
+                                    <div className="scroll-select-news__preview__info">
+                                        <p><img src="img/play_icon.png" alt=""/><img src="img/time-logo.png" alt=""/>15 sec/min ago<span>Rugby Union</span></p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="select-news">
-                                <Link to='/'>Man UTD January Transfer life</Link>
-                                <p><img src="img/time-logo.png" alt=""/>2h<span>Rugby Union</span></p>
+                            <div className="scroll-select-news">
+                                <div className="scroll-select-news__img">
+                                    <img src="img/Minis-landscape.png" alt=""/>
+                                </div>
+                                <div className="scroll-select-news__preview">
+                                    <div className="scroll-select-news__preview__title">
+                                        <h4>Mauricio Pochettino praised Tottenham's</h4>
+                                        <p>opposed the football fan zone on the territory of the university. opposed the football fan zone on the territory of the university.</p>
+                                    </div>
+                                    <div className="scroll-select-news__preview__info">
+                                        <p><img src="img/play_icon.png" alt=""/><img src="img/time-logo.png" alt=""/>15 sec/min ago<span>Rugby Union</span></p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="select-news">
-                                <Link to='/'>Man UTD January Transfer life</Link>
-                                <p><img src="img/time-logo.png" alt=""/>2h<span>Rugby Union</span></p>
+                            <div className="scroll-select-news">
+                                <div className="scroll-select-news__img">
+                                    <img src="img/sporttime.jpg" alt=""/>
+                                </div>
+                                <div className="scroll-select-news__preview">
+                                    <div className="scroll-select-news__preview__title">
+                                        <h4>Mauricio Pochettino praised Tottenham's</h4>
+                                        <p>opposed the football fan zone on the territory of the university. opposed the football fan zone on the territory of the university.</p>
+                                    </div>
+                                    <div className="scroll-select-news__preview__info">
+                                        <p><img src="img/play_icon.png" alt=""/><img src="img/time-logo.png" alt=""/>15 sec/min ago<span>Rugby Union</span></p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="select-news">
-                                <Link to='/'>Man UTD January Transfer life</Link>
-                                <p><img src="img/time-logo.png" alt=""/>2h<span>Rugby Union</span></p>
+                            <div className="scroll-select-news">
+                                <div className="scroll-select-news__img">
+                                    <img src="img/JMP_Bristol_Rugby_v_Bedford_Blues_RT0307.jpg" alt=""/>
+                                </div>
+                                <div className="scroll-select-news__preview">
+                                    <div className="scroll-select-news__preview__title">
+                                        <h4>Mauricio Pochettino praised Tottenham's</h4>
+                                        <p>opposed the football fan zone on the territory of the university. opposed the football fan zone on the territory of the university.</p>
+                                    </div>
+                                    <div className="scroll-select-news__preview__info">
+                                        <p><img src="img/play_icon.png" alt=""/><img src="img/time-logo.png" alt=""/>15 sec/min ago<span>Rugby Union</span></p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="select-news">
-                                <Link to='/'>Man UTD January Transfer life</Link>
-                                <p><img src="img/time-logo.png" alt=""/>2h<span>Rugby Union</span></p>
+                            <div className="scroll-select-news">
+                                <div className="scroll-select-news__img">
+                                    <img src="img/Minis-landscape2.jpg" alt=""/>
+                                </div>
+                                <div className="scroll-select-news__preview">
+                                    <div className="scroll-select-news__preview__title">
+                                        <h4>Mauricio Pochettino praised Tottenham's</h4>
+                                        <p>opposed the football fan zone on the territory of the university. opposed the football fan zone on the territory of the university.</p>
+                                    </div>
+                                    <div className="scroll-select-news__preview__info">
+                                        <p><img src="img/play_icon.png" alt=""/><img src="img/time-logo.png" alt=""/>15 sec/min ago<span>Rugby Union</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="scroll-select-news">
+                                <div className="scroll-select-news__img">
+                                    <img src="img/Minis-landscape.png" alt=""/>
+                                </div>
+                                <div className="scroll-select-news__preview">
+                                    <div className="scroll-select-news__preview__title">
+                                        <h4>Mauricio Pochettino praised Tottenham's</h4>
+                                        <p>opposed the football fan zone on the territory of the university. opposed the football fan zone on the territory of the university.</p>
+                                    </div>
+                                    <div className="scroll-select-news__preview__info">
+                                        <p><img src="img/play_icon.png" alt=""/><img src="img/time-logo.png" alt=""/>15 sec/min ago<span>Rugby Union</span></p>
+                                    </div>
+                                </div>
                             </div>
                         </Scrollbar>
                     </div>
