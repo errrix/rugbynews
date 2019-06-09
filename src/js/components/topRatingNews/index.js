397fd22460586495ce291109551484169fb454a1
Yevhen Ivanov <yevhen.ivanov@ab-soft.net>
2019-06-09 21:42:54 +0300

    mob

--- src/js/components/topRatingNews/index.js
+++ src/js/components/topRatingNews/index.js
@@ -14,7 +14,51 @@ class TopRatingNews extends React.Component {
                         <div className="top-rating-news-block__list__item">
                             <Link to='/'>Man UTD January Transfer life</Link>
                             <div className="top-rating-news-block__info">
-
+                                <p>
+                                    <img src="img/star_icon.png" alt="star_icon"/>
+                                    <img src="img/time_icon.png" alt="time_icon"/>2h<span>Rugby Union</span>
+                                    <img src="img/chat-bubble_color.png" alt="chat-bubble_color"/><span>27</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div className="top-rating-news-block__list__item">
+                            <Link to='/'>Man UTD January Transfer life</Link>
+                            <div className="top-rating-news-block__info">
+                                <p>
+                                    <img src="img/star_icon.png" alt="star_icon"/>
+                                    <img src="img/time_icon.png" alt="time_icon"/>2h<span>Rugby Union</span>
+                                    <img src="img/chat-bubble_color.png" alt="chat-bubble_color"/><span>27</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div className="top-rating-news-block__list__item">
+                            <Link to='/'>Man UTD January Transfer life</Link>
+                            <div className="top-rating-news-block__info">
+                                <p>
+                                    <img src="img/star_icon.png" alt="star_icon"/>
+                                    <img src="img/time_icon.png" alt="time_icon"/>2h<span>Rugby Union</span>
+                                    <img src="img/chat-bubble_color.png" alt="chat-bubble_color"/><span>27</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div className="top-rating-news-block__list__item">
+                            <Link to='/'>Man UTD January Transfer life</Link>
+                            <div className="top-rating-news-block__info">
+                                <p>
+                                    <img src="img/star_icon.png" alt="star_icon"/>
+                                    <img src="img/time_icon.png" alt="time_icon"/>2h<span>Rugby Union</span>
+                                    <img src="img/chat-bubble_color.png" alt="chat-bubble_color"/><span>27</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div className="top-rating-news-block__list__item">
+                            <Link to='/'>Man UTD January Transfer life</Link>
+                            <div className="top-rating-news-block__info">
+                                <p>
+                                    <img src="img/star_icon.png" alt="star_icon"/>
+                                    <img src="img/time_icon.png" alt="time_icon"/>2h<span>Rugby Union</span>
+                                    <img src="img/chat-bubble_color.png" alt="chat-bubble_color"/><span>27</span>
+                                </p>
                             </div>
                         </div>
                     </div>
